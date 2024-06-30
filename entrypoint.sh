484dace0756560153d8a51040567dc24c38733f7
thomas <159631593+thomasjean-louis@users.noreply.github.com>
2024-06-30 17:39:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ cd /var/www/html
 
 #sed -i "s/'quakejs:/window.location.hostname + ':/g" index.html
 
-sed -i "s/'content_server'/':${CONTENT_SERVER}'/g" index.html
-sed -i "s/'game_server'/':${GAME_SERVER}'/g" index.html
+sed -i "s/'content_server'/'${CONTENT_SERVER}'/g" index.html
+sed -i "s/'game_server'/'${GAME_SERVER}'/g" index.html
 
 sed -i "s/':80'/':${HTTP_PORT}'/g" index.html
 
