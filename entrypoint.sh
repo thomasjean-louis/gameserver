40a2a7c45e58afdb41bfc85f6c71d492c261bb8a
thomasjean-louis <159631593+thomasjean-louis@users.noreply.github.com>
2024-07-10 10:28:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ sed -i "s/':80'/':${HTTP_PORT}'/g" index.html
 
 cd /quakejs
 
-node build/ioq3ded.js +set fs_game baseq3 set dedicated 1 +exec server.cfg +set fs_cdn 54.90.25.82
+node build/ioq3ded.js +set fs_game baseq3 set dedicated 1 +exec server.cfg +set fs_cdn localhost
